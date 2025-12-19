146d3d9d2401be2ed664a5ddf43044caf68ec04c
aman Umarvaishya <amanumarvaishya9653021436@gmail.com>
2025-12-19 07:10:10 -0800

    new backend link

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://skillcircles.onrender.com"
+  baseURL: "https://skillcircles-3.onrender.com"
   ,withCredentials:true
 });
 
