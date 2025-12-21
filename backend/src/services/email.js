eb8de9ae465065d5316d75e8e8814a342d7a76bf
aman Umarvaishya <amanumarvaishya9653021436@gmail.com>
2025-12-21 00:31:06 -0800

    email_connection

--- backend/src/services/email.js
+++ backend/src/services/email.js
@@ -5,8 +5,8 @@ dotenv.config();
 export const sendEmail = async (to, subject, message) => {
   try {
     const transporter = nodemailer.createTransport({
-      host: "smtp-relay.brevo.com",
-      port: 587,
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
